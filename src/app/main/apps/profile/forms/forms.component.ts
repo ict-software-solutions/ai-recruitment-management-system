06720eeadfdc7cd4c112f5af28f801183209acd0
jananikarthikeyan94 <jananiflower94@gmail.com>
2020-08-28 10:51:02 +0530

    committed form

--- src/app/main/apps/profile/forms/forms.component.ts
+++ src/app/main/apps/profile/forms/forms.component.ts
@@ -332,7 +332,7 @@ export class FormsComponent implements OnInit, OnDestroy {
 
       this.updateUserSubscription = this.authService.signup(updateObject, this.user).subscribe(
         (res) => {
-          if (res['userId']!== undefined) {
+          if (res === true) {
           if (this.userId === Number(this.userInfo.userId)) {
             if (this.userForm.get("emailAddress").dirty || this.userForm.get("userName").dirty || this.userForm.get("newPassword").dirty) {
               Swal.fire({
