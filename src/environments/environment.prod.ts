c79e3c4e8d9ded2f03663324f381418a3bade10c
Karthiga <karthiga.ravi@diversebrains.com>
2020-11-02 20:27:59 +0530

    environmet files - url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     hmr       : false,
-    url :'http://ec2-3-89-150-233.compute-1.amazonaws.com:3000/'
+    url :'https://q321s2pjyf.execute-api.us-west-1.amazonaws.com/dev/'
 };
